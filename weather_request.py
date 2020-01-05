f189b611afc3f3e46f0814a9d11ff4ea54a7f3f0
kv2709 <kv2709@gmail.com>
2020-01-05 11:36:37 +0700

    Добавлены док-строки

--- weather_request.py
+++ weather_request.py
@@ -7,7 +7,15 @@ FORECAST_URL = "http://api.openweathermap.org/data/2.5/forecast"
 
 
 class WeatherGetter:
+    """
+    Класс выдачи текущей погоды и пятидневного прогноза по любому городу
+    от сервиса openweathermap.org
+    """
     def __init__(self):
+        """
+        Инициализация класса. Токен доступа к АПИ openweathermap.org получается
+        из переменной окружения Heroku, определен в модуле setup
+        """
         self.app_id = APP_ID
         self.weather_url = WEATHER_URL
         self.forecast_url = FORECAST_URL
@@ -17,6 +25,12 @@ class WeatherGetter:
 
     @staticmethod
     def get_wind_direction(wind_deg=0):
+        """
+        Статический метод класса, для преобразования направления ветра,
+        получаемое в градусах от АПИ в символьную нотацию сторон горизонта
+        :param wind_deg: направление ветра в градусах
+        :return: строка в символьной нотации из w_direct
+        """
         w_direct = [' С', 'СВ', ' В', 'ЮВ', ' Ю', 'ЮЗ', ' З', 'СЗ']
         direct_str = ''
         for i in range(0, 8):
@@ -31,6 +45,12 @@ class WeatherGetter:
         return direct_str
 
     def get_current_weather(self, city_id=0):
+        """
+        Метод, отдающий строку текущей погоды по коду города
+        :param city_id: код города
+        :return: строка текущей погоды (дата, температура, направление ветра и
+        состояние(облачность, осадки))
+        """
         try:
             res = requests.get(self.weather_url, params={'id': city_id,
                                                          'units': 'metric',
@@ -51,6 +71,12 @@ class WeatherGetter:
         return weather_str
 
     def get_forecast(self, city_id=0):
+        """
+        Метод, отдающий строку прогноза погоды на пять дней по коду города
+        :param city_id: код города
+        :return: строка прогноза погоды (дата, затем с трех часовым интервалом время,
+        температура, направление ветра и состояние(облачность, осадки))
+        """
         weather_str = ""
         try:
             res = requests.get(FORECAST_URL, params={'id': city_id,

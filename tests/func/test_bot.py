4c21b1a8ab2728edf252034b0acfd6f2065812b8
kv2709 <kv2709@gmail.com>
2020-02-17 18:57:58 +0700

    Тесты готовы

--- tests/func/test_bot.py
+++ tests/func/test_bot.py
@@ -7,7 +7,8 @@ from bot import VKBot
 import vk_api
 import pytest
 
-CMD_LST = [CMD_START,
+CMD_LST = [None,
+           CMD_START,
            CMD_BIYSK_WEATHER_NOW,
            CMD_BIYSK_WEATHER_FORECAST,
            CMD_NOVOSIBIRSK_WEATHER_NOW,
@@ -25,7 +26,8 @@ CMD_LST_revers = [CMD_RETURN_MAIN_MENU,
                   CMD_NOVOSIBIRSK_WEATHER_NOW,
                   CMD_BIYSK_WEATHER_FORECAST,
                   CMD_BIYSK_WEATHER_NOW,
-                  CMD_START]
+                  CMD_START,
+                  None]
 
 
 def test_create_bot_from_my_bot_class(mock_response_requests_get,
@@ -39,19 +41,21 @@ def test_create_bot_from_my_bot_class(mock_response_requests_get,
     for event in bot.vk_bot_pollster.listen(cmd=CMD_START):
         bot.on_event(event=event)
     assert event.object['message']['text'] == 'Начать'
-    assert bot.message_sending == 'Стартовое приветствие'
+    assert bot.user_info[0]['first_name'] == 'Urik'
+    assert bot.message_send_exec_code == 600
 
-# @pytest.mark.parametrize(argnames="cmd", argvalues=CMD_LST)
-# def test_run_bot_from_my_bot_class(cmd,
-#                                    mock_response_requests_get,
-#                                    mock_obj_vk_api,
-#                                    mock_long_poll,
-#                                    mock_vk_api_get,
-#                                    mock_users_get_messages_send,
-#                                    ):
-#     bot = VKBot()
-#     event = None
-#     for event in bot.vk_bot_pollster.listen(cmd=cmd):
-#         bot.on_event(event=event)
-#     assert event.object['message']['text'] == CMD_LST_revers.pop()
-#
+
+@pytest.mark.parametrize(argnames="cmd", argvalues=CMD_LST)
+def test_run_bot_from_my_bot_class(cmd,
+                                   mock_response_requests_get,
+                                   mock_obj_vk_api,
+                                   mock_long_poll,
+                                   mock_vk_api_get,
+                                   mock_users_get_messages_send,
+                                   ):
+    bot = VKBot()
+    event = None
+    for event in bot.vk_bot_pollster.listen(cmd=cmd):
+        bot.on_event(event=event)
+    assert event.object['message']['text'] == CMD_LST_revers.pop()
+    assert bot.message_send_exec_code == 600
